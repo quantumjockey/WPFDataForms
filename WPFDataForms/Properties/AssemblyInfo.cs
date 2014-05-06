a12124326cf5a89eea754647e24bac8cd4048c6c
quantumjockey <nickyd242@gmail.com>
2014-05-05 17:14:36 -0700

    Edited UML model definition for completeness.

--- WPFDataForms/Properties/AssemblyInfo.cs
+++ WPFDataForms/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WPFDataForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for quickly generating and manipulating data forms within WPF application interfaces.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nicola B. DiPalma")]
 [assembly: AssemblyProduct("WPFDataForms")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 Nicola B. DiPalma")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
